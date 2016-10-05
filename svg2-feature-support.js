725c58b1802d97944bf7eb20e6b906e9fc18b1c5
Nikos Andronikos <nikos.andronikos@cisra.canon.com.au>
2016-10-05 17:29:42 +1100

    Show feature support levels

--- svg2-feature-support.js
+++ svg2-feature-support.js
@@ -1,5 +1,6 @@
 (function() {
 
+
 function $(selector) {
     return document.querySelector(selector);
 }
@@ -20,29 +21,71 @@ function httpGetAsync(theUrl, callback, callback_data, page)
 }
 
 function features_json_received(responseText) {
-    var categories = JSON.parse(responseText);
-    for (category of categories) {
+    const response = JSON.parse(responseText);
+    const num_cols = response.user_agents.length + 2;
+    const table_head_row = $('#feature-support-table > thead > tr');
+
+    for (ua of response.user_agents) {
+        const th = new_elem('th');
+        th.className = 'UA';
+        th.innerHTML = ua;
+        table_head_row.appendChild(th);
+    }
+
+
+    for (let category of response.svg2_categories) {
         const tbody = new_elem('tbody');
         const tr = new_elem('tr');
-        const td = new_elem('td');
 
-        td.innerHTML = category.category;
+        tr.className = 'category';
+        tr.innerHTML = `<td colspan=${num_cols}>${category.category}</td>`;
 
-        tr.appendChild(td);
         tbody.appendChild(tr);
         $('#feature-support-table').appendChild(tbody);
         
-        for (feature of category.features) {
+        for (let feature of category.features) {
             const tr = new_elem('tr');
-            const td = new_elem('td');
-            td.innerHTML = feature.name;
-            tr.appendChild(td);
+            tr.innerHTML = `<td class='feature'>${feature.name}</td><td></td>`;
             tbody.appendChild(tr);
+            
+            for (let ua of response.user_agents) {
+                const td = new_elem('td');
+                td.id = ua;
+                td.className = 'UA none';
+                tr.appendChild(td);
+            }
+            
+            for (let ua_support of feature.support) {
+                const ua_td = tr.querySelector(`#${ua_support.name}`);
+                switch (ua_support.level) {
+                    case 'full': 
+                    case 'partial':
+                        ua_td.innerHTML = ua_support.level;
+                        ua_td.className = ua_support.level;
+                        break;
+                    case 'N/A':
+                        ua_td.innerHTML = ua_support.level;
+                        ua_td.className = 'not_applicable';
+                        break;
+                }
+
+                if (ua_support.tracker) {
+                    function tracker_links(tracker) {
+                        let resolved = '';
+                        for (t of Array.isArray(tracker) ? tracker : [tracker]) {
+                            resolved += ` [<a href='${t}'>track</a>]`;
+                        }
+                        return resolved;
+                    }
+                    ua_td.innerHTML = ua_td.innerHTML + tracker_links(ua_support.tracker);
+                }
+            }
         }
     }
 }
 
 window.onload = function() {
+
     httpGetAsync(
         'svg2-new-features.json',
         features_json_received
